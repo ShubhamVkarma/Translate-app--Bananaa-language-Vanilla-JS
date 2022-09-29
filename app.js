02df9ab0bf370cb4d6167435c485f2ad6b6a43d5
Shubham Vishwakarma <svkarma8@gmail.com>
2022-09-30 00:37:26 +0530

    error handling and formatting

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ var inputtext = document.querySelector("#textarea1")
 var outputtext = document.querySelector("#output")
 var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-function translate() {
-    var api = url + "?" +"text="+ inputtext.value;
-    fetch(api).then(response=>response.json()).then(json=> console.log(json));
+function errorhandler(error) {
+    alert("An error occured with server")
+    console.log("error occured:"+error)
+}
 
-    fetch(api).then(response=>response.json()).then(json=> outputtext.innerHTML= json.contents.translated );
+function translate() {
+  
+    var api = url + "?" +"text="+ inputtext.value;
+
+    //This is API call 
+    // fetch(api).then(response=>response.json()).then(json=> console.log(json)); 
+    
+    fetch(api).
+    then(response=>response.json()).
+    then(json=> {
+        var translatedtext = json.contents.translated;
+        outputtext.innerText= translatedtext }).
+    catch(errorhandler)
 }
 
 btntranslate.addEventListener("click", translate)
